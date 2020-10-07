7c9c43fd7892cf0e837be55038935e88d577b955
hgianotitone <henriquegianoti@gmail.com>
2020-10-07 06:16:33 +0200

    tidyverse install

--- data/twosillyvectors.R
+++ data/twosillyvectors.R
@@ -1,3 +1,7 @@
 num <- c(1,2,3,4)
 num2 <- c(0,1,2,3)
-data.frame(num,num2)
+sillyplot <-data.frame(num,num2)
+install.packages(tidyverse)
+library("tidyverse")
+ggplot(sillyplot)+
+  geom_point(aes(x=num,y=num2))
